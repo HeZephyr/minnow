c176f36110c3bf475031e9a7d4afe00dc87f6e05
HeZephyr <2825841950@qq.com>
2025-04-13 19:38:47 +0800

    feat: finish the webget of lab0

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,25 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
+  // Create a TCP socket and connect to the server.
+  TCPSocket socket;
+  socket.connect(Address(host, "http"));
+  string request = "GET " + path + " HTTP/1.1\r\n" + 
+                    "Host: " + host + "\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n";
+  socket.write(request);
+
+  string response;
+  while (!socket.eof()) {
+    string buffer;
+    socket.read(buffer);
+    response += buffer;
+  }
+  cout << response;
+  // Close the socket.
+  socket.close();
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main( int argc, char* argv[] )
